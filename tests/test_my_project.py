160818da7bb45b7375644da2a33fc5bfa36c1914
Ethan Benabou <ethanbnb@gmail.com>
2021-12-02 21:59:46 -0500

    Add demo selenium test
    - Adds selenium and a demo test to show how to work with selenium.
    - Adds CI job for running selenium tests
    
    Closes FLAP-116

--- tests/test_my_project.py
+++ tests/test_my_project.py
@@ -1,5 +1,29 @@
-from my_project import __version__
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.remote.webdriver import WebDriver
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_python_dot_org(driver: WebDriver):
+    """This is a demo test that shows basic usage of selenium.
+
+    Args:
+        driver (WebDriver): The WebDriver test fixture. Injected by pytest.
+    """
+
+    driver.get("https://www.python.org")
+
+    # Enter a search
+    ele = driver.find_element(By.TAG_NAME, "#id-search-field")
+    ele.send_keys(Keys.TAB)
+    ele.clear()
+    ele.send_keys("python3")
+
+    # Submit the search
+    driver.find_element(By.CSS_SELECTOR, "#submit").click()
+
+    # Grab the first search result
+    search_results = driver.find_element(By.CSS_SELECTOR, ".list-recent-events")
+    first_result = search_results.find_element(By.CSS_SELECTOR, "a")
+
+    # Assert that the first search result is some text...
+    assert first_result.text == 'PEP 394 -- The "python" Command on Unix-Like Systems'
