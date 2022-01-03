f06ef12930cfce8dc517b745c82efdd29308a1b7
Ethan Benabou <ethanbnb@gmail.com>
2022-01-02 20:01:19 -0500

    [FLAP-116] bootstrap gui testing repo (#2)
    * Create new poetry project
    
    Bootstrap the project
    
    Related FLAP-116
    
    * Add demo selenium test
    
    - Adds selenium and a demo test to show how to work with selenium.
    - Adds CI job for running selenium tests
    
    Closes FLAP-116

---
+++ options.py
@@ -0,0 +1,14 @@
+"""
+WebDriver options
+
+Feel free to tweak the different options as needed. For example, you might like
+to comment out the `--headless` option while you are writing/debugging your
+tests in order to see what is happening in chrome during the test.
+"""
+
+from selenium import webdriver
+
+OPTIONS = webdriver.ChromeOptions()
+
+# Comment this out while debugging to see what is going on in Chrome
+OPTIONS.add_argument("--headless")
