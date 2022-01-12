ef55fea783e5490ce7455c88847c6256cb8a71bb
thenafsters <nafisa.shamsuzzaman@gmail.com>
2022-01-12 13:38:22 -0500

    Removed Commented lines

--- tests/test_run_inputs.py
+++ tests/test_run_inputs.py
@@ -41,21 +41,3 @@ def test_shadow_input_tape(driver: WebDriver):
     # ele = driver.find_element(By.CLASS_NAME, "ant-modal-body").get_attribute("value")
     true_element = "Write your input tape here"
     assert ele == true_element
-
-
-# FIX THIS TEST
-# def test_run_add_input_tape(driver: WebDriver):
-#     driver.get("https://machinist.flapflap.io/")
-#     driver.find_element_by_xpath('//*[@id="__next"]/main/div[2]/ul/li[2]').click()
-
-#     # enter an input tape
-#     ele = driver.find_element(By.CLASS_NAME, "ant-input")
-#     ele.send_keys(Keys.TAB)
-#     ele.clear()
-#     ele.send_keys("tape 1")
-
-#     true_element = "TypeError: Failed to fetch"
-#     WebDriverWait(driver, 5)
-#     driver.find_element_by_class_name("ant-btn ant-btn-primary").click()
-#     element_inspect = driver.find_element_by_class_name("ant-modal-body").text
-#     assert element_inspect == true_element
