ed6a0542163b5cc486c35807d9ac7fc7bde8df85
Amneet  Singh <amneet.s.270@gmail.com>
2022-01-17 14:37:50 -0500

    FLAP-122 [Machinist-UI-Testing] System test - verify q0 node is not added automatically
    this test check if the bug FLAP-82 (Random q0 state is created when the app is started) has been fixed
    
    closes FLAP-122

---
+++ tests/test_bugs.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+
+
+MAIN_PAGE = "https://machinist.flapflap.io/"
+
+
+def test_no_node_present_at_start(driver: WebElement):
+
+    driver.maximize_window()
+    driver.get(MAIN_PAGE)
+
+    node_elements = driver.find_elements(
+        By.XPATH, r'.//*[contains(@class,"State__StateRoot")]'
+    )
+    #assert len(node_elements) == 1
