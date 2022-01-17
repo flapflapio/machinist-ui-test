36472386e18cb7b2db700df94f5d130e9ede58fc
Amneet  Singh <amneet.s.270@gmail.com>
2022-01-17 07:57:15 -0500

    FLAP-117 System tests Menu bar testing and debugger testing
    changes made according to reviews:
    -removed the demo test
    -reenabled headless in options.py

--- tests/test_machinist.py
+++ tests/test_machinist.py
@@ -13,32 +13,6 @@ LOGGER = logging.getLogger(__name__)
 MAIN_PAGE = "https://machinist.flapflap.io/"
 
 
-def test_python_dot_org(driver: WebDriver):
-    """This is a demo test that shows basic usage of selenium.
-
-    Args:
-        driver (WebDriver): The WebDriver test fixture. Injected by pytest.
-    """
-
-    driver.get("https://www.python.org")
-
-    # Enter a search
-    ele = driver.find_element(By.TAG_NAME, "#id-search-field")
-    ele.send_keys(Keys.TAB)
-    ele.clear()
-    ele.send_keys("python3")
-
-    # Submit the search
-    driver.find_element(By.CSS_SELECTOR, "#submit").click()
-
-    # Grab the first search result
-    search_results = driver.find_element(By.CSS_SELECTOR, ".list-recent-events")
-    first_result = search_results.find_element(By.CSS_SELECTOR, "a")
-
-    # Assert that the first search result is some text...
-    assert first_result.text == 'PEP 394 -- The "python" Command on Unix-Like Systems'
-
-
 def test_expanding_menu_bar(driver: WebDriver):
     driver.maximize_window()
     driver.get(MAIN_PAGE)
